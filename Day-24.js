ebc11276619fd4127288bcca1012c69c902873ed
manasakokkalakonda <manasak2810@gmail.com>
2025-08-18 19:47:17 +0530

    Add files via upload

---
+++ Day-24.js
@@ -0,0 +1,23 @@
+//100Daysofcode
+//Day24
+
+function lengthOfLongestSubstring(s) {
+    let charSet = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
+        }
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    
+    return maxLength;
+}
+console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+console.log(lengthOfLongestSubstring("bbbbb"));    // Output: 1
+console.log(lengthOfLongestSubstring("pwwkew"));   // Output: 3
+console.log(lengthOfLongestSubstring(""));         // Output: 0
